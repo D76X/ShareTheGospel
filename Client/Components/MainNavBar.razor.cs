95813a383018bf4b12e1e72d1b3aca1b119566ef
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2023-01-01 19:37:06 +0100

    Replaced the local component LanguageSelector.razor with the same component placed into a separate component library Websites.Razor.ClassLibrary.Components + saved the solution file MyProject.sln as Websites.sl into the folder SahredTheGospel that is a GIT repo in order to trackl its changes over time. Removed unused SharedTheGospel.sln  Resolved: #29

---
+++ Client/Components/MainNavBar.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Client.Components
+{
+	public class MainNavBarBase : ComponentBase
+	{
+		//protected const string CountryCodeUk = "GB";
+		//protected const string CountryCodeGermany = "DE";
+		//protected const string CountryCodeItaly = "IT";
+	}
+}
