f53f66f0ebaa8a83d10775ec30891e6ab3ac35fc
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2023-04-27 12:22:17 +0200

    bsic implementation with language switch GB/en, DE, IT

--- Client/Components/MainNavBar.razor.cs
+++ Client/Components/MainNavBar.razor.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp.Client.Components
 {
 	public class MainNavBarBase : ComponentBase
 	{
-		//protected const string CountryCodeUk = "GB";
-		//protected const string CountryCodeGermany = "DE";
-		//protected const string CountryCodeItaly = "IT";
+		protected const string CountryCodeUk = "gb";
+		protected const string CountryCodeGermany = "de";
+		protected const string CountryCodeItaly = "it";
+
+		[Inject]
+		public NavigationManager NavigationManager { get; set; }
+
+		public void LanguageSelected(string selectedLanguage)
+		{
+			var baseUri = NavigationManager.BaseUri;
+			var currentUri = NavigationManager.Uri;
+			if(currentUri==baseUri) return;
+
+			if (currentUri == $"{baseUri}#")
+			{
+				NavigationManager.NavigateTo($"{baseUri}{selectedLanguage}", forceLoad: true);
+			};
+			
+
+			if (currentUri.EndsWith($"/{selectedLanguage}")) return;
+
+			if (currentUri.EndsWith("/en") ||
+			    currentUri.EndsWith("/de") ||
+			    currentUri.EndsWith("/it"))
+			{
+				currentUri = currentUri.Substring(0, currentUri.Length - 3);
+			}
+			
+			var nextUri = $"{currentUri}/{selectedLanguage}";
+			NavigationManager.NavigateTo(nextUri, forceLoad: true);
+		}
 	}
 }
