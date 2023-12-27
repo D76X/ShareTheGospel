2e0cd19fe41bf5d977f046d5662bea76ab77c150
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2023-12-27 12:53:51 +0100

    improved navigation and laguage selection

--- Client/Components/MainNavBar.razor.cs
+++ Client/Components/MainNavBar.razor.cs
@@ -1,33 +1,36 @@
-﻿using System.Net.Http.Headers;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Websites.Razor.ClassLibrary.Components;
 
 namespace BlazorApp.Client.Components
 {
 	public class MainNavBarBase : ComponentBase
 	{
-		protected const string CountryCodeUk = "gb";
-		protected const string CountryCodeGermany = "de";
-		protected const string CountryCodeItaly = "it";
-
-		[Inject]
+        [Inject]
 		public NavigationManager NavigationManager { get; set; }
 
-		public void LanguageSelected(string selectedLanguage)
+        public void LinkSelected(string toPage)
+        {
+            var nextUri = $"{toPage}/{LanguageSelectorBase.SelectedLanguage}";
+            NavigationManager.NavigateTo(nextUri);
+        }
+
+        public void LanguageSelected(string selectedLanguage)
 		{
 			var baseUri = NavigationManager.BaseUri;
+
 			var currentUri = NavigationManager.Uri;
-			if(currentUri==baseUri) return;
+            if (currentUri.EndsWith($"/{selectedLanguage}")) return;
 
-			if (currentUri.EndsWith($"/{selectedLanguage}")) return;
-
-			if (currentUri.EndsWith("/en") ||
-			    currentUri.EndsWith("/de") ||
-			    currentUri.EndsWith("/it"))
+			if (currentUri.EndsWith($"/{LanguageSelectorBase.LanguageEn}") ||
+			    currentUri.EndsWith($"/{LanguageSelectorBase.LanguageDe}") ||
+			    currentUri.EndsWith($"/{LanguageSelectorBase.LanguageIt}"))
 			{
 				currentUri = currentUri.Substring(0, currentUri.Length - 3);
 			}
-			
-			var nextUri = $"{currentUri}/{selectedLanguage}";
+
+            if (currentUri.EndsWith("/")) currentUri = currentUri.Substring(0, currentUri.Length - 1); 
+
+            var nextUri = $"{currentUri}/{selectedLanguage}";
 			NavigationManager.NavigateTo(nextUri);
 			
 		}
