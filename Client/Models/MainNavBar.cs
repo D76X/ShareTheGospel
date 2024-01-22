51e4fcd2a67b729feca92f4c9d8f6b6b15f18642
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-01-22 14:56:12 +0100

    MenuNavBar model

---
+++ Client/Models/MainNavBar.cs
@@ -0,0 +1,27 @@
+﻿using BlazorApp.Client.Translations;
+using Websites.Razor.ClassLibrary.Components;
+
+namespace BlazorApp.Client.Models
+{
+    public class MainNavBar
+    {
+        public static string KeyAbout = PageTranslations.About.ToLower();
+
+        public string SelectedLanguage { get; set; }
+
+        public MenuItem About => Models[KeyAbout][SelectedLanguage];
+
+        public Dictionary<string, Dictionary<string, MenuItem>> Models = new()
+        {
+            {
+                KeyAbout,
+                new Dictionary<string, MenuItem>()
+                {
+                    { LanguageSelectorBase.LanguageEn, new MenuItem(PageTranslations.About, PageTranslations.About) },
+                    { LanguageSelectorBase.LanguageDe, new MenuItem(PageTranslations.About, PageTranslations.About) },
+                    { LanguageSelectorBase.LanguageIt, new MenuItem(PageTranslations.About, PageTranslations.About) },
+                }
+            }
+        };
+    }
+}
