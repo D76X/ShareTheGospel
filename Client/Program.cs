65942a4f2901e656599a02584968494b724e94c1
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-01-22 14:19:01 +0100

    service and models registrations

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,12 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using BlazorApp.Client.Abstractions.Models;
+using BlazorApp.Client.Abstractions.Services;
+using BlazorApp.Client.Pages.Models;
+using BlazorApp.Client.Services.LanguageService;
 using BlazorApp.Client.Services.LocalStorage;
 using BlazorApp.Client.Services.UserSettingsService;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,8 +14,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddSingleton<ILanguageService, LanguageService>();
 builder.Services.AddSingleton<IUserSettingsService, UserSettingsService>();
-
 builder.Services.AddTransient<ILocalStorageService, LocalStorageService>();
+
+builder.Services.AddTransient<IAboutModel,AboutModel>();
 
 await builder.Build().RunAsync();
