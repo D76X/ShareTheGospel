c3ece96eae7703312817326a4439c89905796a84
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-01-22 15:00:11 +0100

    CaModels/Ctalog replaces MenuCatalog

---
+++ Client/Pages/Models/Catalog.cs
@@ -0,0 +1,17 @@
+﻿using BlazorApp.Client.Translations;
+
+namespace BlazorApp.Client.Pages.Models;
+
+public static class Catalog
+{
+    public const string Tour = @"Tour";
+    public const string KeyAbout = @"About";
+
+    public static Dictionary<string, string[]> Translations = new()
+    {
+        { Tour, new []{@"Tour", @"Tour", @"Visita" }},
+        { KeyAbout, new []{@"About", @"Infos", @"Informazioni" }},
+    };
+
+    public static string Translation(string id, string language) => Translator.Get(id, language, Translations);
+}
