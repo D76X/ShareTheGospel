70b13a8878b818d56f0bad9cb170f7f21d717c2b
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-01-22 14:07:10 +0100

    LanguageService event based implementation

---
+++ Client/Abstractions/Services/ILanguageService.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp.Client.Abstractions.Services;
+
+public interface ILanguageService
+{ 
+    string? SelectedLanguage { get; set; }
+    event EventHandler<string>? SelectedLanguageChanged;
+}
---
+++ Client/Services/LanguageService/LanguageService.cs
@@ -0,0 +1,28 @@
+﻿
+using BlazorApp.Client.Abstractions.Services;
+
+namespace BlazorApp.Client.Services.LanguageService
+{
+    public class LanguageService: ILanguageService
+    {
+        private string? _selectedLanguage;
+
+        public string? SelectedLanguage
+        {
+            get => _selectedLanguage;
+
+            set
+            {
+                _selectedLanguage = value;
+                OnSelectLanguage(value);
+            }
+        }
+
+        public event EventHandler<string>? SelectedLanguageChanged;
+
+        protected virtual void OnSelectLanguage(string? e)
+        {
+            SelectedLanguageChanged?.Invoke(this, e);
+        }
+    }
+}
