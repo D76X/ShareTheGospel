9772652ccd1b44730ddf8c4ec044431d382f01b1
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-01-22 14:54:31 +0100

    MenuItem model

---
+++ Client/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using BlazorApp.Client.Abstractions.Models;
+
+namespace BlazorApp.Client.Models
+{
+    public class MenuItem : IMenuItem
+    {
+        public string Href { get; }
+        public string Text { get; }
+
+        public MenuItem(
+            string href,
+            string text)
+        {
+            Href = href;
+            Text = text;
+        }
+    }
+}
