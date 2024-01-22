e1e3444ddeae6846c80040e3f73a79abe0b987b7
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-01-22 14:18:17 +0100

    About.razor, AboutModel and IAboutModel with event subscription to ILanguageService

---
+++ Client/Abstractions/Models/IAboutModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.Client.Abstractions.Models;
+
+public interface IAboutModel: IDisposable
+{
+    public event Action OnLanguageSelection;
+    string SelectedLanguage { get; }
+    bool IsEnglish { get; }
+    bool IsDeutsch { get; }
+    bool IsItalian { get; }
+}
---
+++ Client/Pages/Models/AboutModel.cs
@@ -0,0 +1,45 @@
+﻿using BlazorApp.Client.Abstractions.Models;
+using BlazorApp.Client.Abstractions.Services;
+using Websites.Razor.ClassLibrary.Components;
+
+namespace BlazorApp.Client.Pages.Models
+{
+    public class AboutModel: IAboutModel
+    {
+        /// <summary>
+        /// the service that notifies this model about the selections of languages made by the user on the UI
+        /// </summary>
+        private readonly ILanguageService _languageService;
+
+        /// <summary>
+        /// the event that this model raises to notify the corresponding component StateHasChanged
+        /// that it is time to redraw the UI as the model has changed
+        /// </summary>
+        public event Action? OnLanguageSelection;
+
+        public AboutModel(ILanguageService languageService)
+        {
+            _languageService = languageService;
+            SelectedLanguage = LanguageSelectorBase.SelectedLanguage;
+            _languageService.SelectedLanguageChanged += OnSelectedLanguageChanged;
+        }
+
+        private void OnSelectedLanguageChanged(
+            object? sender, 
+            string e)
+        {
+            SelectedLanguage = e;
+            OnLanguageSelection?.Invoke();
+        }
+
+        public string SelectedLanguage { get; set; }
+        public bool IsEnglish => SelectedLanguage == LanguageSelectorBase.LanguageEn;
+        public bool IsDeutsch => SelectedLanguage == LanguageSelectorBase.LanguageDe;
+        public bool IsItalian => SelectedLanguage == LanguageSelectorBase.LanguageIt;
+
+        public void Dispose()
+        {
+            _languageService.SelectedLanguageChanged -= OnSelectedLanguageChanged;
+        }
+    }
+}
