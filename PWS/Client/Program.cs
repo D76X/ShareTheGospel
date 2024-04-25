2ca1897e9ed4ea11313aeecf717114d9d96cafa0
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-04-25 11:40:16 +0200

    PWS Client/Program.cs

--- PWS/Client/Program.cs
+++ PWS/Client/Program.cs
@@ -1,15 +1,19 @@
 using Client;
+using Client.Abstractions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+//using AzureStaticWebApps.Blazor.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-{
-    //BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-    BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress)
-});
+builder.Services
+	.AddScoped(sp => new HttpClient
+	{
+		BaseAddress = new Uri(builder.Configuration[Constants.ApiPrefix] ??
+		                      builder.HostEnvironment.BaseAddress)
+	});
+	//.AddStaticWebAppsAuthentication();
 
 await builder.Build().RunAsync();
