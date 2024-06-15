18364714d1cd3e61ad2bad3fe4fbe065919c8d19
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-06-15 15:26:45 +0200

    STG fixed missing test property on IndexModel

--- STG/Client/Pages/Models/Pages/IndexModel.cs
+++ STG/Client/Pages/Models/Pages/IndexModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Websites.Razor.ClassLibrary.Abstractions.Models;
 using Websites.Razor.ClassLibrary.Abstractions.Services;
 
@@ -6,9 +7,13 @@ namespace Client.Pages.Models.Pages
 {
     public class IndexModel : IIndexPage
     {
-        private readonly ILanguageService _languageService;
         private readonly ICardService _cardService;
+        private readonly ILanguageService _languageService;
         private readonly ISearchService _searchService;
+
+        public string TestString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event Action? OnStateHasChanged;
 
         public IndexModel(
             ILanguageService languageService,
