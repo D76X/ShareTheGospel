e8ce6692864142db587ae6f62e34c329dd6a9d00
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-10-13 14:14:20 +0200

    test CardBase_Is_Searchabble CASE-2

--- TestProject1/SearchResultTest.cs
+++ TestProject1/SearchResultTest.cs
@@ -3,6 +3,7 @@ using Websites.Razor.ClassLibrary.Abstractions;
 using Websites.Razor.ClassLibrary.Abstractions.Models;
 using Websites.Razor.ClassLibrary.Abstractions.Services;
 using Websites.Razor.ClassLibrary.Models;
+// ReSharper disable InconsistentNaming
 
 namespace Websites.Razor.ClassLibrary.Test
 {
@@ -94,8 +95,6 @@ namespace Websites.Razor.ClassLibrary.Test
             results1.Type.Should().Be<SearchableBase>();
             results1.TypeStr.Should().Be(typeStrTestCard);
             results1.SearchResults.Should().NotBeEmpty();
-
-            // ?
             results1.Value.Should().NotBeNull();
             results1.Value.Should().BeOfType<SearchableBase>();
 
@@ -118,30 +117,84 @@ namespace Websites.Razor.ClassLibrary.Test
             match1Value.Should().NotBe(cardModelWithKeyword1);
 
             // the remaining 2 card models are not matches for this keyword
-            var noMatches = results1.SearchResults.Where(i => !i.IsMatch).ToArray();
-            noMatches.Should().AllBeOfType<NullSearchResult>();
-            var noMatch0 = noMatches[0];
-            var noMatch1 = noMatches[1];
-            noMatch0.SearchTerm.Should().Be(searchTerm1);
-            noMatch1.SearchTerm.Should().Be(searchTerm1);
+            var noMatches1 = results1.SearchResults.Where(i => !i.IsMatch).ToArray();
+            noMatches1.Should().AllBeOfType<NullSearchResult>();
+            var noMatch1_0 = noMatches1[0];
+            var noMatch1_1 = noMatches1[1];
+            noMatch1_0.SearchTerm.Should().Be(searchTerm1);
+            noMatch1_1.SearchTerm.Should().Be(searchTerm1);
             
-            var noMatchesValues = noMatches
+            var noMatchesValues1 = noMatches1
                 .Select(i => i.Value as CardModel)
                 .OrderBy(i => i!.PageRef)
                 .ToArray();
-            noMatchesValues.Should().HaveCount(2);
-            var noMatchesValuesKeyword2 = noMatchesValues.Single(i => i!.PageText.Contains(searchTerm2));
-            var noMatchesValuesKeyword3 = noMatchesValues.Single(i => i!.PageText.Contains(searchTerm3));
+            noMatchesValues1.Should().HaveCount(2);
+            var noMatchesValues1Keyword2 = noMatchesValues1.Single(i => i!.PageText.Contains(searchTerm2));
+            var noMatchesValues1Keyword3 = noMatchesValues1.Single(i => i!.PageText.Contains(searchTerm3));
 
-            noMatchesValuesKeyword2.Should().BeEquivalentTo(cardModelWithKeyword2);
-            noMatchesValuesKeyword2.Should().NotBe(cardModelWithKeyword2);
+            noMatchesValues1Keyword2.Should().BeEquivalentTo(cardModelWithKeyword2);
+            noMatchesValues1Keyword2.Should().NotBe(cardModelWithKeyword2);
 
-            noMatchesValuesKeyword3.Should().BeEquivalentTo(cardModelWithKeyword3);
-            noMatchesValuesKeyword3.Should().NotBe(cardModelWithKeyword3);
+            noMatchesValues1Keyword3.Should().BeEquivalentTo(cardModelWithKeyword3);
+            noMatchesValues1Keyword3.Should().NotBe(cardModelWithKeyword3);
 
             // -----------------------------------------------------------------------------------------------
             // CASE-2: there is a match for search term 2 in the page text of one of the models of the card
 
+            // act 
+            var results2 = searchableTestCard.GetResult(searchTerm2);
+
+            // assert
+            results1.Should().NotBeNull();
+            // there is a match for the card
+            results2.Should().BeOfType<MatchSearchResult>();
+            results2.IsMatch.Should().BeTrue();
+            results2.SearchTerm.Should().Be(searchTerm2);
+            results2.Type.Should().Be<SearchableBase>();
+            results2.TypeStr.Should().Be(typeStrTestCard);
+            results2.SearchResults.Should().NotBeEmpty();
+            results2.Value.Should().NotBeNull();
+            results2.Value.Should().BeOfType<SearchableBase>();
+
+            // there is a match for the card which has 3 searchables in it 
+            // these searchables are its 3 contained card models
+            results2.SearchResults.Should().HaveCount(3);
+            var typeStrSearchResults2 = results2.SearchResults.Select(i => i.TypeStr).ToArray();
+            typeStrSearchResults2.Should().AllBe(typeStrCardModel);
+
+            // there is only one matching card 
+            var match2 = results2.SearchResults.Single(i => i.IsMatch);
+            match2.Should().BeOfType<MatchSearchResult>();
+            match2.SearchTerm.Should().Be(searchTerm2);
+            match2.Type.Should().Be<CardModel>();
+            match2.TypeStr.Should().Be(typeStrCardModel);
+            var match2Value = match2.Value as CardModel;
+            match2Value.Should().NotBeNull();
+            match2Value!.PageText.Should().Contain(searchTerm2);
+            match2Value.Should().BeEquivalentTo(cardModelWithKeyword2);
+            match2Value.Should().NotBe(cardModelWithKeyword2);
+
+            // the remaining 2 card models are not matches for this keyword
+            var noMatches2 = results2.SearchResults.Where(i => !i.IsMatch).ToArray();
+            noMatches2.Should().AllBeOfType<NullSearchResult>();
+            var noMatch2_0 = noMatches2[0];
+            var noMatch2_1 = noMatches2[1];
+            noMatch2_0.SearchTerm.Should().Be(searchTerm2);
+            noMatch2_1.SearchTerm.Should().Be(searchTerm2);
+
+            var noMatchesValues2 = noMatches2
+                .Select(i => i.Value as CardModel)
+                .OrderBy(i => i!.PageRef)
+                .ToArray();
+            noMatchesValues2.Should().HaveCount(2);
+            var noMatchesValues2Keyword1 = noMatchesValues2.Single(i => i!.PageText.Contains(searchTerm1));
+            var noMatchesValues2Keyword3 = noMatchesValues2.Single(i => i!.PageText.Contains(searchTerm3));
+
+            noMatchesValues2Keyword1.Should().BeEquivalentTo(cardModelWithKeyword1);
+            noMatchesValues2Keyword1.Should().NotBe(cardModelWithKeyword1);
+
+            noMatchesValues2Keyword3.Should().BeEquivalentTo(cardModelWithKeyword3);
+            noMatchesValues2Keyword3.Should().NotBe(cardModelWithKeyword3);
 
             // -----------------------------------------------------------------------------------------------
             // CASE-3: there is a match for search term 3 in the page text of one of the models of the card
