a96cae639f53d634869ea173dd59e3d00f4c81da
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2024-12-11 11:23:45 +0100

    moved swa.ps1 to 'C:\VSProjects\MyProjetcs\Websites\Sites' and edited to work with new switch -site

--- swa.ps1
+++ swa.ps1
@@ -17,12 +17,21 @@
     [int]$sslport=7249,
 
     # the port where the Azure Function API is deployed locally to the Azure Function Runtime 
-    # this works only when CORS is enabled for the AF Api - refer to teh READ.me for details  
+    # this works only when CORS is enabled for the AF Api - refer to the README for details  
     [Parameter(Mandatory=$false)]
-    [int]$apiport=7174
+    [int]$apiport=7174,
+
+    [Parameter(Mandatory=$false)]
+    [string]$site='PWS'
+    # [string]$location='C:\VSProjects\MyProjetcs\Websites\Sites\PWS'
 )
 
-# the following three lines perfrom some cleanup in the Api folder 
+$location='C:\VSProjects\MyProjetcs\Websites\Sites\PWS'
+if($site -eq 'PWS') {$location='C:\VSProjects\MyProjetcs\Websites\Sites\PWS'}
+if($site -eq 'STG') {$location='C:\VSProjects\MyProjetcs\Websites\Sites\STG'}
+Set-Location $location
+
+# the following three lines perform some cleanup in the Api folder 
 # the obj and bin folder in the Api folder must be clean otherwise
 # the Azure Function runtime fails to run the API for the static web app
 # more details are in the READ.me
@@ -31,9 +40,8 @@ if(Test-Path obj) {Remove-Item obj -Recurse}
 if(Test-Path bin) {Remove-Item bin -Recurse}
 
 # This part goes back to the folder that contains the three folders for
-# client app, the api app and the sharedd folder and there then it stops 
-# any running jobs that may have been started by previous runs of this 
-# script
+# client app, the api app and the shared folder and there it stops any 
+# running jobs that may have been started by previous runs of this script
 Set-Location ..
 Get-Job | Where-Object state -ne running | Group-Object -Property Name
 Get-Job | Where-Object state -ne running | Remove-Job
@@ -66,19 +74,17 @@ if ($opentab) {
 # https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-run
 
 # swa start allows you to start the blazor application with or without Hot Reload.
-# This is controlled solely by the command that is passed to the --run option.
+# This is controlled solely by the command that is passed to the --run option
 # of the swa start command. Hot Reload is only possible if the [dotnet watch run]
 # that is the watch switch is used.
 
 # The Blazor Web Dev Server is the default Dev Server for Blazor.
 # It runs by default at the fixed EP http://localhost:5000 and this cannot be changed.
 # Only at the EP exposed by the Blazor Web Dev Server Hot Reload is possible.
-# If you choose to use lauch your app on a port different from 5000 
-# for example: http://localhost:5015 
-# then Hot Reload will not be available on this EP (http://localhost:5015) whether the 
-# the [dotnet watch run] or the [dotnet run] is used bacause this EP is not the EP of 
-# the Blazor Web Dev Server
-# then there will be no hot reload there as that EOP is not the EP of the 
+# If you choose to launch your app on a port different from 5000 for example: 
+# http://localhost:5015 then Hot Reload will not be available on this EP regardless
+# whether the [dotnet watch run] or the [dotnet run] is used bacause this EP is not 
+# the EP of the Blazor Web Dev Server.
 
 if ($ssl) {  
   # Launch with hot reload and SSL: https://localhost:7249
@@ -91,3 +97,5 @@ else{
   #swa start http://localhost:5000 --run "dotnet watch run --launch-profile http --project Client/Client.csproj" --api-location Api --api-port 7174  
   swa start http://localhost:$port --run "dotnet watch run --launch-profile http --project Client/Client.csproj" --api-location Api --api-port $apiport  
 }
+
+Set-Location 'C:\VSProjects\MyProjetcs\Websites\Sites'
